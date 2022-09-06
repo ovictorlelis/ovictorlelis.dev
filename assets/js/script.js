1f2ef2a331e84bea69ed16447ad1eaec10d36d8a
Victor Lelis <ovictorlelis@gmail.com>
2022-09-06 07:52:19 -0300

    fix mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,42 @@
-let links = document.querySelectorAll("nav ul li a");
-let sections = document.querySelectorAll("section");
+tabs();
+menuMobile();
 
-links.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
+function tabs() {
+  let links = document.querySelectorAll("nav ul li a");
+  let sections = document.querySelectorAll("section");
 
-    links.forEach(function (link) {
-      link.classList.remove("active");
+  links.forEach(function (link) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      links.forEach(function (link) {
+        link.classList.remove("active");
+      });
+
+      sections.forEach(function (section) {
+        section.classList.add("none");
+      });
+
+      let id = link.getAttribute("href").replace("#", "");
+      let current = document.querySelector("#" + id);
+      link.classList.add("active");
+      current.classList.remove("none");
     });
-
-    sections.forEach(function (section) {
-      section.classList.add("none");
-    });
-
-    let id = link.getAttribute("href").replace("#", "");
-    let current = document.querySelector("#" + id);
-    link.classList.add("active");
-    current.classList.remove("none");
   });
-});
+}
+
+function menuMobile() {
+  let menu = document.querySelector("nav button");
+  let menuItems = document.querySelectorAll("nav ul li");
+  let ul = document.querySelector("nav ul");
+
+  menu.addEventListener("click", function () {
+    ul.classList.toggle("active");
+  });
+
+  menuItems.forEach(function (items) {
+    items.addEventListener("click", function () {
+      ul.classList.remove("active");
+    });
+  });
+}
