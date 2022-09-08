43227d6b72c4128dc4baeade303a8d302a1aceb4
Victor Lelis <ovictorlelis@gmail.com>
2022-09-07 22:56:44 -0300

    alguns ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 tabs();
 menuMobile();
 
+getContent();
+
 function tabs() {
   let links = document.querySelectorAll("nav ul li a");
   let sections = document.querySelectorAll("section");
@@ -45,4 +47,169 @@ function menuMobile() {
       ul.classList.remove("active");
     });
   });
+}
+
+function getContent() {
+  let loading = document.querySelector("#loading");
+  let error = document.querySelector("#error");
+  let page = document.querySelector("#page");
+
+  let portfolioContent = document.querySelector("#portfolio");
+  let sobreContent = document.querySelector("#sobre");
+
+  let portfolio = [];
+  let sobre = [];
+
+  fetch(
+    "https://api.github.com/repos/ovictorlelis/ovictorlelis.github.io/issues"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((items) => {
+        switch (items.labels[0].name) {
+          case "portfolio":
+            portfolio.push(items);
+            break;
+          case "sobre":
+            sobre.push(items);
+            break;
+        }
+      });
+
+      let regex = /!?\[([^\]]*)\]\(([^\)]+)\)/gm;
+      portfolio.forEach((data) => {
+        let matches = data.body.match(regex);
+        let content = data.body.replace(regex, "").trim();
+        let datas = [];
+        const singleMatch = /\[([^\[]+)\]\((.*)\)/;
+        for (var i = 0; i < matches.length; i++) {
+          var text = singleMatch.exec(matches[i]);
+          datas[text[1]] = text[2];
+        }
+        let url = new URL(datas["github"]);
+
+        fetch("https://api.github.com/repos" + url.pathname + "/languages")
+          .then((response) => response.json())
+          .then((languages) => {
+            let language = Object.keys(languages);
+
+            portfolioContent.innerHTML += `
+            <article>
+              <img src="${datas["cover"]}" alt="Print do projeto" />
+              <div class="content">
+                  <h3>${data.title}</h3>
+                  <p class="info">
+                    ${content}
+                  </p>
+                  <p class="codes">
+                    <span>${language.join(" | ")}</span>
+                    <span>
+                      <a
+                        target="_blank"
+                        href="${datas["link"]}"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="192"
+                          height="192"
+                          fill="var(--color-feature)"
+                          viewBox="0 0 256 256"
+                        >
+                          <rect width="256" height="256" fill="none"></rect>
+                          <path
+                            d="M122.3,71.4l19.8-19.8a44.1,44.1,0,0,1,62.3,62.3l-28.3,28.2a43.9,43.9,0,0,1-62.2,0"
+                            fill="none"
+                            stroke="var(--color-feature)"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="16"
+                          ></path>
+                          <path
+                            d="M133.7,184.6l-19.8,19.8a44.1,44.1,0,0,1-62.3-62.3l28.3-28.2a43.9,43.9,0,0,1,62.2,0"
+                            fill="none"
+                            stroke="var(--color-feature)"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="16"
+                          ></path>
+                        </svg>
+                      </a>
+                    </span>
+                    <span>
+                      <a
+                        target="_blank"
+                        href="${datas["github"]}"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="192"
+                          height="192"
+                          fill="var(--color-feature)"
+                          viewBox="0 0 256 256"
+                        >
+                          <rect width="256" height="256" fill="none"></rect>
+                          <path
+                            d="M84,240a23.9,23.9,0,0,0,24-24V168"
+                            fill="none"
+                            stroke="var(--color-feature)"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="16"
+                          ></path>
+                          <path
+                            d="M172,240a23.9,23.9,0,0,1-24-24V168"
+                            fill="none"
+                            stroke="var(--color-feature)"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="16"
+                          ></path>
+                          <path
+                            d="M152,168h16a23.9,23.9,0,0,1,24,24v8a23.9,23.9,0,0,0,24,24"
+                            fill="none"
+                            stroke="var(--color-feature)"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="16"
+                          ></path>
+                          <path
+                            d="M104,168H88a23.9,23.9,0,0,0-24,24v8a23.9,23.9,0,0,1-24,24"
+                            fill="none"
+                            stroke="var(--color-feature)"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="16"
+                          ></path>
+                          <path
+                            d="M111.8,64A52,52,0,0,0,68,40a52,52,0,0,0-3.5,44.7A49.3,49.3,0,0,0,56,112v8a48,48,0,0,0,48,48h48a48,48,0,0,0,48-48v-8a49.3,49.3,0,0,0-8.5-27.3A52,52,0,0,0,188,40a52,52,0,0,0-43.8,24Z"
+                            fill="none"
+                            stroke="var(--color-feature)"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            stroke-width="16"
+                          ></path>
+                        </svg>
+                      </a>
+                    </span>
+                  </p>
+                </div>
+              </article>
+            `;
+          });
+      });
+
+      const converter = new showdown.Converter();
+      sobreContent.innerHTML = converter.makeHtml(sobre[0].body);
+
+      loading.classList.remove("flex");
+      loading.classList.add("none");
+      page.classList.remove("none");
+    })
+    .catch((err) => {
+      loading.classList.remove("flex");
+      loading.classList.add("none");
+
+      error.classList.add("flex");
+      error.classList.remove("none");
+    });
 }
